654b04509d3454b50b18b34a30b7c6ac9c7df556
Said Jahangirov <saidjahangirov@Saids-MacBook-Air.local>
2023-05-02 17:27:03 +0400

    created Task 1

---
+++ Practise 8/Program.cs
@@ -0,0 +1,6 @@
+﻿using Practise_8.Utilities;
+
+string word = "Salam Said Salam Salam";
+string searchTerm = Console.ReadLine();
+
+Console.WriteLine(word.MatchCount(searchTerm));
---
+++ Practise 8/Utilities/Extension.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Practise_8;
+namespace Practise_8.Utilities
+{
+	public static class Extension
+	{
+		public static int MatchCount(this string word, string searchTerm)
+		{
+			int count = 0;
+			string[] occurences = word.Split(" ");
+			for (int i = 0; i < occurences.Length; i++)
+			{
+				if (occurences[i] == searchTerm)
+				{
+					count++;
+				}
+			}
+			return count;
+
+		}
+	}
+}
+

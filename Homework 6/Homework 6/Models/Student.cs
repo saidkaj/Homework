4c6ac721b58e73367d33aaa0c1fe89081d24fe27
Said Jahangirov <saidjahangirovv@gmail.com>
2023-05-02 17:31:19 +0400

    Add files via upload

---
+++ Homework 6/Homework 6/Models/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace Homework_6.Models
+{
+	public class Student
+	{
+		private static int objectCount = 0;
+		public int Id { get; private set; }
+
+		public Student()
+		{
+			objectCount++;
+			Id = objectCount;	
+		}
+	}
+}
+
+
---
+++ Homework 6/Homework 6/Program.cs
@@ -0,0 +1,12 @@
+﻿using Homework_6.Models;
+
+
+Student stu1 = new Student();
+Student stu2 = new Student();
+Student stu3 = new Student();
+
+
+
+Console.WriteLine(stu1.Id);
+Console.WriteLine(stu2.Id);
+ 
